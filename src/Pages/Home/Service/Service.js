5147111f9f8df990315f01afe2cc2e0ef7f188c4
Rahman Rakib <rahmanrakibur6589@gmail.com>
2022-03-24 00:06:39 +0600

    card design updated

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,14 +1,29 @@
 import React from 'react';
+import { Button, Card } from 'react-bootstrap';
+import './Service.css'
 
 const Service = ({ x }) => {
      const { name, image, description } = x;
      return (
-          <div>
-               <img src={image} alt="" />
-               <h1>{name}</h1>
-               <h4>{description}</h4>
+          <div className='w-100'>
+               <div className='service w-100'>
+                    <Card style={{ border: 'none' }} className='shadow-lg'>
+                         <Card.Img variant="top" src={image} />
+                         <Card.Body>
+                              <Card.Title>{name}</Card.Title>
+                              <Card.Text>
+                                   {description}
+                              </Card.Text>
+                         </Card.Body>
+                    </Card>
+               </div>
           </div>
+
      );
 };
 
 export default Service;
+
+
+
+
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
+import { Container } from 'react-bootstrap';
 import Service from '../Service/Service';
 import './Services.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faTruck } from '@fortawesome/free-solid-svg-icons';
 
 const Services = () => {
      const [service, setService] = useState([]);
@@ -15,13 +18,19 @@ const Services = () => {
      }, [])
 
      return (
-          <div>
-               <div className='services-container'>
-                    {
-                         service.map(x => <Service x={x} key={x._id}></Service>)
-                    }
+          <Container>
+               <div style={{ marginTop: '10%' }}>
+                    <header style={{ textAlign: 'center' }}>
+                         <h1>OUR <b>SERVICES</b></h1>
+                         ----- <FontAwesomeIcon icon={faTruck} fontSize='2em' style={{ color: 'red' }} /> -----
+                    </header>
+                    <div className='services-container mt-5'>
+                         {
+                              service.map(x => <Service x={x} key={x._id}></Service>)
+                         }
+                    </div>
                </div>
-          </div>
+          </Container>
      );
 };
 
